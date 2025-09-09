dc7257bb612b3f89d81e79daa7d54244922604cc
abyad-tif <lapajung50@gmail.com>
2025-09-09 20:17:39 +0800

    blog system

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('home', ['title' => 'Home Page']);
+});
+
+Route::get('/posts', function () {
+    $posts = Post::all();
+    return view('posts', ['title' => 'Blog', 'posts' => $posts]);
+});
+
+Route::get('/post/{post:slug}', function (Post $post) {
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
+});
+
+Route::get('/authors/{user:username}', function (User $user) {
+    return view('posts', ['title' => count($user->post) . ' Posts from ' . $user->name, 'posts' => $user->post]);
+});
+
+Route::get('/about', function () {
+    return view('about', ['title' => 'About']);
+});
+
+Route::get('/contact', function () {
+    return view('contact', ['title' => 'Contact']);
+});
