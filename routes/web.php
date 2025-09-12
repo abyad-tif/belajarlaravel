a2c43e695ba8423f8f062c5a69ab7570db8f4b1b
abyad-tif <lapajung50@gmail.com>
2025-09-12 17:14:56 +0800

    pagination

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ Route::get('/', function () {
 
 Route::get('/posts', function () {
     // $posts = Post::with(['author', 'category'])->latest()->get()
-    $posts = Post::latest()->filter(request(['search', 'category', 'author']))->get();
+    $posts = Post::latest()->filter(request(['search', 'category', 'author']))->paginate(6)->withQueryString();
   
     return view('posts', ['title' => 'Blog', 'posts' => $posts]);
 });
